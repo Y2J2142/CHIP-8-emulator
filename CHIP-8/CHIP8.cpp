361a17157cc1337c1798991ad1485cae4988b63a
Unknown <Y2J2142@hotmail.com>
2018-07-02 23:55:34 +0200

    more opcodes added

--- CHIP-8/CHIP8.cpp
+++ CHIP-8/CHIP8.cpp
@@ -28,12 +28,85 @@ void CHIP8::emulateCycle()
 
 	switch (opcode & 0xF000)
 	{
-	case 0x1000:
-		pc = opcode & 0x0FFF;
-		break;
-	case 0xA000:
-		I = opcode & 0x0FFF;
-		pc += 2;
+		case 0x1000:
+			pc = opcode & 0x0FFF;
+			break;
+		case 0x2000:
+			stack[++sp] = pc;
+			pc = opcode & 0x0FFF;
+			break;
+		case 0x3000:
+			if (V[opcode & 0x0F00] == opcode & 0x00FF)
+				pc += 2;
+			pc += 2;
+			break;
+		case 0x4000:
+			if (V[opcode & 0x0F00] != opcode & 0x00FF)
+				pc += 2;
+			pc += 2;
+			break;
+		case 0x5000:
+			if (V[opcode & 0x0F00] == V[0x00F0])
+				pc += 2;
+			pc += 2;
+			break;
+		case 0x6000:
+			V[opcode & 0x0F00] = 0x00FF;
+			pc += 2;
+			break;
+		case 0x7000:
+			V[opcode & 0x0F00] += opcode & 0x00FF;
+			pc += 2;
+			break;
+		case 0x8000:
+			switch (opcode & 0x000F)
+			{
+				case 0x0000:
+					V[opcode & 0x0F00] = V[opcode & 0x00F0];
+					pc += 2;
+					break;
+				case 0x0001:
+					V[opcode & 0x0F00] = V[opcode & 0x0F00] | V[opcode & 0x00F0];
+					pc += 2;
+					break;
+				case 0x0002:
+					V[opcode & 0x0F00] = V[opcode & 0x0F00] & V[opcode & 0x00F0];
+					pc += 2;
+					break;
+				case 0x0003:
+					V[opcode & 0x0F00] = V[opcode & 0x0F00] ^ V[opcode & 0x00F0];
+					pc += 2;
+					break;
+				case 0x0004:
+					if (V[opcode & 0x0F00] + V[opcode & 0x00F0] > 255)
+						V[0xF] = 1;
+					V[opcode & 0x0F00] = V[opcode & 0x0F00] + V[opcode & 0x00F0];
+					pc += 2;
+					break;
+				case 0x0005:
+					V[0xF] = V[opcode & 0x0F00] > V[opcode & 0x00F0];
+					V[opcode & 0x0F00] = V[opcode & 0x0F00] - V[opcode & 0x00F0];
+					pc += 2;
+					break;
+				case 0x0006:
+					V[0xF] = V[0x0F00] & 1;
+					V[0x0F00] /= 2;
+					pc += 2;
+					break;
+				case 0x0007:
+					V[0xF] = V[0x00F0] > V[0x0F00];
+					V[0x0F00] = V[0x00F0] - V[0x0F0];
+					pc += 2;
+					break;
+				case 0x000E:
+					V[0xF] = V[0x0F00] & 0b10000000
+
+
+			}
+			break;
+		case 0xA000:
+			I = opcode & 0x0FFF;
+			pc += 2;
 		break;
 	}
 }
