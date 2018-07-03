671d5b27bdc73c56ad608346d6b8738dfe98cb49
Unknown <Y2J2142@hotmail.com>
2018-07-03 20:39:37 +0200

    gfx made into 2d array

--- CHIP-8/CHIP8.cpp
+++ CHIP-8/CHIP8.cpp
@@ -6,14 +6,15 @@ using std::begin;
 using std::end;
 using std::copy_n;
 
-
 void CHIP8::initialize()
 {
 	pc = 0x200;
 	opcode = 0;
 	I = 0;
 	sp = 0;
-	fill(begin(gfx), end(gfx), false);
+	for (auto &  ar : gfx)
+		fill(begin(ar), end(ar), false);
+	
 	fill(begin(stack), end(stack), 0);
 	fill(begin(V), end(V), 0);
 	fill(begin(memory), end(memory), 0);
@@ -119,6 +120,9 @@ void CHIP8::emulateCycle()
 			break;
 		case 0xC000:
 			srand(time(NULL));
+			V[opcode & 0x0F00] = (rand() % 256) & (opcode & 0x00FF);
+			break;
+
 	}
 }
 
--- CHIP-8/CHIP8.h
+++ CHIP-8/CHIP8.h
@@ -8,7 +8,7 @@ struct CHIP8
 	uint8_t  memory[4096];
 	uint8_t V[16];
 	uint16_t I, pc;
-	bool gfx[64 * 32];
+	bool gfx[32][64];
 	uint8_t delay_timer, sound_timer;
 	uint16_t stack[16];
 	uint16_t sp;
